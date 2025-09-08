7124bf8b6162048794b95a9c2e455d5356c7da72
Sergiy4ik <sergiy063l@gmail.com>
2025-09-08 12:10:09 +0300

    js-hw-06

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,31 @@
-function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    return contentValue + paddingValue * 2 + borderValue * 2;
+class StringBuilder {
+    #value
+    constructor(params) {
+        this.#value = params
+    }
+    getValue() {
+        return this.#value
+    }
+    padStart(str) {
+        this.#value = str + this.#value
+    }
+    padEnd(str) {
+        this.#value += str
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str
+    }
+
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
